5ef68eac20fbbe41289157e46317e3b562914665
Claudio Teles <telesapps85@gmail.com>
2022-05-03 13:35:57 -0400

    sliding text working

--- src/app/components/sliding-texts/sliding-texts.component.ts
+++ src/app/components/sliding-texts/sliding-texts.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild, } from '@angular/core';
 
 @Component({
   selector: 'app-sliding-texts',
@@ -7,16 +7,34 @@ import { AfterViewInit, Component, OnInit, } from '@angular/core';
 })
 
 export class SlidingTextsComponent implements OnInit, AfterViewInit {
-  
 
+  @ViewChild('content', {static: false}) private testDiv?: ElementRef<HTMLDivElement>;
+  isTestDivScrolledIntoView: boolean = false
+
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView(){
+    if (this.testDiv){
+      const rect = this.testDiv.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      this.isTestDivScrolledIntoView = topShown && bottomShown;
+    }
+    console.log('isVIsible', this.isTestDivScrolledIntoView);
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-
+    setTimeout(() => {
+      console.log('isVIsible', this.isTestDivScrolledIntoView);
+    }, 2000);
   }
+
+  isHidden(el: any) {
+    return (el.offsetParent === null)
+}
 
 
 }
